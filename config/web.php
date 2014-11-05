b5d4fd73bbd7ed96eb18a8bd755d79f41e4ad50a
Vasilii <letov.rst@gmail.com>
2014-11-06 02:21:14 +0400

    Test cookie validation key added. Code coverage integration.

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'TEST_COOKIE_VALIDATION_KEY',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
